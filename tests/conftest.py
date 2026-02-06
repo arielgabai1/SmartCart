e2fea799d3e3ecdcc86d94bbf9e10373cca898e0
ArielGabai1 <arielgabai555@gmail.com>
2026-02-06 21:35:52 +0200

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,2 @@
-"""
-Test configuration - minimal setup since integration tests use real services.
-"""
 import os
-
-# Set test environment variables
 os.environ['JWT_SECRET'] = 'test-secret-value'
